d5debe1c3c84f1563402ff56bcd3e04ce77e9ba8
ChaosTyphon <kenjikaido123@gmail.com>
2021-09-16 11:34:17 +0700

    comit s

--- Assets/New Folder/dichuyen.cs
+++ Assets/New Folder/dichuyen.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class dichuyen : MonoBehaviour
 {
     public float speed;
+    public float Jumpshigh;
+    bool facingright;
+    bool grounded;
 
     Rigidbody2D body;
     Animator anim;
@@ -13,13 +16,53 @@ public class dichuyen : MonoBehaviour
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        facingright = true;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("Run", Mathf.Abs(move));
         body.velocity = new Vector2(move * speed, body.velocity.y);
 
+        // dieu kien cua flip de co the cho nhan vat quay mat ra sau lung
+        if (move > 0 && !facingright)
+            {
+                flip();
+            }
+        else if (move < 0 && facingright)
+            {
+                flip();
+            }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                body.velocity = new Vector2(body.velocity.x, Jumpshigh);
+
+            }
+        }
+        
     }
+    void flip()
+        {// code cho nhan vat quay mat ra sau lung khi di lui
+            facingright = !facingright;
+            Vector3 Thescale = transform.localScale;
+            Thescale.x *= -1;
+            transform.localScale = Thescale;
+        }
+    void OnCollisionEnter2D(Collision2D other )
+    {
+        if(other.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+
 }
+
